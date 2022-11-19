f567a4f3806a40bc2b8a8f118cbdf22081ab45a8
Gabriel Pereira <ggonpereira@gmail.com>
2022-11-19 10:31:23 -0300

    ♻️ chore: add expo-font and configure fonts on app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    'GeneralSans-400': require('./assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  if (!isFontsLoaded) return null;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
