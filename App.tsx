f7729ef83f0243a9696a37789caaa45709911403
Gabriel Pereira <ggonpereira@gmail.com>
2022-11-19 11:18:56 -0300

    🎉 feat: component Main initialized

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
+import { Main } from './Main';
 
 export default function App() {
   const [isFontsLoaded] = useFonts({
@@ -11,19 +10,5 @@ export default function App() {
 
   if (!isFontsLoaded) return null;
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Main />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#00aacc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Main/Main.tsx
@@ -0,0 +1,10 @@
+import * as S from './Main.styles';
+import { Text } from '../components/Text';
+
+export const Main = () => {
+  return (
+    <S.Container>
+      <Text>Renderizou</Text>
+    </S.Container>
+  );
+};
